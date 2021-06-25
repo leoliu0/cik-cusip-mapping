650f015d6617bf2e338f73b6bd41fffe775a1fac
Leo Liu <auleo.liu@gmail.com>
2021-06-25 15:58:45 +1000

    not capture IRS

--- parse_cusip.py
+++ parse_cusip.py
@@ -9,48 +9,63 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('files')
+parser.add_argument('--debug', action='store_true')
+
 args = parser.parse_args()
 
-# pat = re.compile('[\( >][0-9A-Z]{1}[0-9]{4}[0-9A-Z]{1}\s*[0-9]{0,2}\s*[0-9]{0,1}[\) \n<]')
 pat = re.compile(
-    '[\( >][0-9A-Z]{1}[0-9]{3}[0-9A-Za-z]{2}[- ]*[0-9]{0,2}[- ]*[0-9]{0,1}[\) \n<]'
+    '[\( >]*[0-9A-Z]{1}[0-9]{3}[0-9A-Za-z]{2}[- ]*[0-9]{0,2}[- ]*[0-9]{0,1}[\) \n<]*'
 )
 w = re.compile('\w+')
 
 
 def parse(file):
     with open(file, 'r') as f:
-        a = f.readlines()
+        lines = f.readlines()
 
     record = 0
     cik = None
-    for x in a:
-        if 'SUBJECT COMPANY' in x:
+    for line in lines:
+        if 'SUBJECT COMPANY' in line:
             record = 1
-        if 'CENTRAL INDEX KEY' in x and record == 1:
-            cik = x.split('\t\t\t')[-1].strip()
+        if 'CENTRAL INDEX KEY' in line and record == 1:
+            cik = line.split('\t\t\t')[-1].strip()
             break
 
     cusips = []
     record = 0
-    for x in a:
-        if '<DOCUMENT>' in x:
+    for line in lines:
+        if '<DOCUMENT>' in line:  # lines are after the document preamble
             record = 1
         if record == 1:
-            y = pat.findall(x)
-            if y:
-                cusips.append(y[0].strip().strip('<>'))
-
+            if 'IRS' not in line and 'I.R.S' not in line:
+                fd = pat.findall(line)
+                if fd:
+                    cusip = fd[0].strip().strip('<>')
+                    if args.debug:
+                        print('INFO: added --- ', line, " --- extracted [",
+                              cusip, "]")
+                    cusips.append(cusip)
     if len(cusips) == 0:
         cusip = None
     else:
         cusip = Counter(cusips).most_common()[0][0]
         cusip = ''.join(w.findall(cusip))
+    if args.debug:
+        print(cusip)
 
     return [file, cik, cusip]
 
 
 def main():
+    if args.debug:
+        path = Path(args.files)
+        if path.exists():
+            print(parse(args.files))
+        else:
+            raise ValueError("provide a single file to debug ...")
+        return
+
     with Pool(30) as p:
         with open(args.files + '.csv', 'w') as w:
             wr = csv.writer(w)
