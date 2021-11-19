0f3836902cdeccebaeda14fe1954b5d70cb8eb0e
Leo Liu <auleo.liu@gmail.com>
2021-11-19 12:39:58 +1100

    add user agent for SEC requirement

--- dl_idx.py
+++ dl_idx.py
@@ -8,15 +8,15 @@ import requests
 user_agent = {'User-agent': 'Mozilla/5.0'}
 
 if __name__=='__main__':
-    # with open(f'master.idx', 'wb') as f:
-    #     for year in range(1994, 2023):
-    #         for q in range(1, 5):
-    #             print(year, q)
-    #             content = requests.get(
-    #                 f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{q}/master.idx",
-    #                 headers=user_agent
-    #             ).content
-    #             f.write(content)
+    with open(f'master.idx', 'wb') as f:
+        for year in range(1994, 2023):
+            for q in range(1, 5):
+                print(year, q)
+                content = requests.get(
+                    f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{q}/master.idx",
+                    headers=user_agent
+                ).content
+                f.write(content)
 
     with open('full_index.csv', 'w', errors='ignore') as csvfile:
         wr = csv.writer(csvfile)
