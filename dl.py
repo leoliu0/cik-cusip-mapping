926a0e85a02b2b6e21c1e99d1c16d4326add2967
Leo Liu <auleo.liu@gmail.com>
2021-11-19 12:39:38 +1100

    add user agent for SEC requirement

--- dl.py
+++ dl.py
@@ -6,47 +6,48 @@ import os
 import pandas as pd
 import requests
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filing', type=str)
-parser.add_argument('folder', type=str)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filing', type=str)
+    parser.add_argument('folder', type=str)
 
-args = parser.parse_args()
-filing = args.filing
-folder = args.folder
+    args = parser.parse_args()
+    filing = args.filing
+    folder = args.folder
 
-full = pd.read_csv('full_index.csv', encoding='latin1')
+    full = pd.read_csv('full_index.csv', encoding='latin1')
 
-form = full[full.form.str.contains(filing)]
+    form = full[full.form.str.contains(filing)]
 
-len_ = len(form)
-print(len_)
-print('start to download')
+    len_ = len(form)
+    print(len_)
+    print('start to download')
 
-try:
-    os.mkdir(folder)
-except:
-    pass
-
-for n, row in enumerate(form.values):
-    print(f'{n} out of {len_}')
-    cik = row[0]
-    firm = row[1]
-    date = row[3]
-    year = date.split('-')[0]
-    month = date.split('-')[1]
-    url = row[4].strip()
     try:
-        os.mkdir(f'./{folder}/{year}_{month}')
+        os.mkdir(folder)
     except:
         pass
-    if os.path.exists(f'./{folder}/{year}_{month}/{cik}_{date}.html'):
-        continue
-    try:
-        txt = requests.get(f'https://www.sec.gov/Archives/{url}',
-                           timeout=60).text
-        with open(f'./{folder}/{year}_{month}/{cik}_{date}.html',
-                  'w',
-                  errors='ignore') as f:
-            f.write(txt)
-    except:
-        print(f'{cik}, {date} failed to download')
+
+    for n, row in enumerate(form.values):
+        print(f'{n} out of {len_}')
+        cik = row[0]
+        firm = row[1]
+        date = row[3]
+        year = date.split('-')[0]
+        month = date.split('-')[1]
+        url = row[4].strip()
+        try:
+            os.mkdir(f'./{folder}/{year}_{month}')
+        except:
+            pass
+        if os.path.exists(f'./{folder}/{year}_{month}/{cik}_{date}.html'):
+            continue
+        try:
+            txt = requests.get(f'https://www.sec.gov/Archives/{url}',
+                               timeout=60).text
+            with open(f'./{folder}/{year}_{month}/{cik}_{date}.html',
+                      'w',
+                      errors='ignore') as f:
+                f.write(txt)
+        except:
+            print(f'{cik}, {date} failed to download')
--- dl_idx.py
+++ dl_idx.py
@@ -5,19 +5,23 @@ import os
 import pandas as pd
 import requests
 
-with open(f'master.idx', 'wb') as f:
-    for year in range(1994, 2023):
-        for q in range(1, 5):
-            print(year, q)
-            content = requests.get(
-                f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{q}/master.idx"
-            ).content
-            f.write(content)
+user_agent = {'User-agent': 'Mozilla/5.0'}
 
-with open('full_index.csv', 'w', newline='', errors='ignore') as csvfile:
-    wr = csv.writer(csvfile)
-    wr.writerow(['cik', 'comnam', 'form', 'date', 'url'])
-    with open('master.idx', 'r', encoding='latin1') as f:
-        for r in f:
-            if '.txt' in r:
-                wr.writerow(r.split('|'))
+if __name__=='__main__':
+    # with open(f'master.idx', 'wb') as f:
+    #     for year in range(1994, 2023):
+    #         for q in range(1, 5):
+    #             print(year, q)
+    #             content = requests.get(
+    #                 f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{q}/master.idx",
+    #                 headers=user_agent
+    #             ).content
+    #             f.write(content)
+
+    with open('full_index.csv', 'w', errors='ignore') as csvfile:
+        wr = csv.writer(csvfile)
+        wr.writerow(['cik', 'comnam', 'form', 'date', 'url'])
+        with open('master.idx', 'r', encoding='latin1') as f:
+            for r in f:
+                if '.txt' in r:
+                    wr.writerow(r.strip().split('|'))
