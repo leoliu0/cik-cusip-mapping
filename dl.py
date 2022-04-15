46812af08ab598e52481dc2b250a1ebba21c9ef3
Leo Liu <auleo.liu@gmail.com>
2022-04-15 10:19:19 +1000

    user_agent

--- dl.py
+++ dl.py
@@ -6,22 +6,24 @@ import os
 import pandas as pd
 import requests
 
-if __name__=='__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('filing', type=str)
-    parser.add_argument('folder', type=str)
+    parser.add_argument("filing", type=str)
+    parser.add_argument("folder", type=str)
+
+    user_agent = {"User-agent": "Mozilla/5.0"}
 
     args = parser.parse_args()
     filing = args.filing
     folder = args.folder
 
-    full = pd.read_csv('full_index.csv', encoding='latin1')
+    full = pd.read_csv("full_index.csv", encoding="latin1")
 
     form = full[full.form.str.contains(filing)]
 
     len_ = len(form)
     print(len_)
-    print('start to download')
+    print("start to download")
 
     try:
         os.mkdir(folder)
@@ -29,25 +31,26 @@ if __name__=='__main__':
         pass
 
     for n, row in enumerate(form.values):
-        print(f'{n} out of {len_}')
+        print(f"{n} out of {len_}")
         cik = row[0]
         firm = row[1]
         date = row[3]
-        year = date.split('-')[0]
-        month = date.split('-')[1]
+        year = date.split("-")[0]
+        month = date.split("-")[1]
         url = row[4].strip()
         try:
-            os.mkdir(f'./{folder}/{year}_{month}')
+            os.mkdir(f"./{folder}/{year}_{month}")
         except:
             pass
-        if os.path.exists(f'./{folder}/{year}_{month}/{cik}_{date}.html'):
+        if os.path.exists(f"./{folder}/{year}_{month}/{cik}_{date}.html"):
             continue
         try:
-            txt = requests.get(f'https://www.sec.gov/Archives/{url}',
-                               timeout=60).text
-            with open(f'./{folder}/{year}_{month}/{cik}_{date}.html',
-                      'w',
-                      errors='ignore') as f:
+            txt = requests.get(
+                f"https://www.sec.gov/Archives/{url}", headers=user_agent, timeout=60
+            ).text
+            with open(
+                f"./{folder}/{year}_{month}/{cik}_{date}.html", "w", errors="ignore"
+            ) as f:
                 f.write(txt)
         except:
-            print(f'{cik}, {date} failed to download')
+            print(f"{cik}, {date} failed to download")
