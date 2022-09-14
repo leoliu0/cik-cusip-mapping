a8b595a5705e88139ce645caf34b0c60356cae32
Leo Liu <auleo.liu@gmail.com>
2022-09-14 18:14:43 +1000

    user_agent and some refactoring

--- dl_idx.py
+++ dl_idx.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import argparse
 import csv
 import os
@@ -5,23 +6,23 @@ import os
 import pandas as pd
 import requests
 
-user_agent = {'User-agent': 'Mozilla/5.0'}
+user_agent = {"User-agent": "Mozilla/5.0"}
 
-if __name__=='__main__':
-    with open(f'master.idx', 'wb') as f:
+if __name__ == "__main__":
+    with open(f"master.idx", "wb") as f:
         for year in range(1994, 2023):
             for q in range(1, 5):
                 print(year, q)
                 content = requests.get(
                     f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{q}/master.idx",
-                    headers=user_agent
+                    headers=user_agent,
                 ).content
                 f.write(content)
 
-    with open('full_index.csv', 'w', errors='ignore') as csvfile:
+    with open("full_index.csv", "w", errors="ignore") as csvfile:
         wr = csv.writer(csvfile)
-        wr.writerow(['cik', 'comnam', 'form', 'date', 'url'])
-        with open('master.idx', 'r', encoding='latin1') as f:
+        wr.writerow(["cik", "comnam", "form", "date", "url"])
+        with open("master.idx", "r", encoding="latin1") as f:
             for r in f:
-                if '.txt' in r:
-                    wr.writerow(r.strip().split('|'))
+                if ".txt" in r:
+                    wr.writerow(r.strip().split("|"))
